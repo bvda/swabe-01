0b48fae50047761c87c01550d91ff5f9bd7e94dc
Brian Vestergaard Danielsen <brian.v.danielsen@gmail.com>
2022-01-23 19:18:05 +0100

    Add basic Express example

---
+++ examples/hello-express/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.json({
+    'message': 'Hello, Express.js!'
+  });
+});
+
+app.listen(port, _ => {
+  console.debug(`Server running on port ${port}`);
+});

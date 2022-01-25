af1536075da80ab3e901e0c35d15eed34be5e3af
Brian Vestergaard Danielsen <bvd@ase.au.dk>
2022-01-25 16:23:05 +0100

    Fix string

--- examples/hello-node/server.js
+++ examples/hello-node/server.js
@@ -10,7 +10,7 @@ const server = http.createServer((req, res) => {
     console.debug(req.headers)
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({'message': 'Hello, world'}));
+    res.end(JSON.stringify({'message': 'Hello, Node.js'}));
   }
   if(req.method == 'POST') {
     let body = ''

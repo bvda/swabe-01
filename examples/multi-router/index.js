86200bb48146580e6c77acb57611c1a8180a48bf
Brian Vestergaard Danielsen <bvd@ase.au.dk>
2022-01-28 11:02:34 +0100

    Add routing content and examples

--- examples/multi-router/index.js
+++ examples/multi-router/index.js
@@ -1,15 +1,24 @@
 // Adapted from https://github.com/expressjs/express/blob/master/examples/multi-router/index.js
 
 const express = require('express');
-const port = 3000;
+const bodyParser = require('body-parser');
 
+const port = 3000;
 const app = express();
+
+app.use(bodyParser.text());
 
 app.use('/api/v1', require('./controllers/api_v1'));
 app.use('/api/v2', require('./controllers/api_v2'));
 
 app.get('/', function(req, res) {
-  res.send('Hello from root route.')
+  res.send('Hello from root route.');
+});
+
+app.post('/', function(req, res) {
+  res.json({
+    message: req.body
+  });
 });
 
 app.listen(port, _ => {

0988d2d3f247cfb50b29d357cf501fe8bd95e451
Brian Vestergaard Danielsen <bvd@ase.au.dk>
2022-01-31 13:55:46 +0100

    Moved some stuff around

---
+++ examples/hello-node/methods-and-routes.js
@@ -0,0 +1,47 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  if(req.url === '/') {
+    if(req.method === 'GET') {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Hello, Node.js');
+    }
+    if(req.method === 'POST') {
+      let body = ''
+      req.on('data', chunk => {
+        body += chunk
+      });
+      req.on('end', _ => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end(body)
+      });
+    }
+  } else if(req.url === '/json') {
+    if(req.method === 'GET') {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify({'message': 'Hello, Node.js'}));
+    }
+    if(req.method === 'POST') {
+      let body = ''
+      req.on('data', chunk => {
+        body += chunk
+      });
+      req.on('end', _ => {
+        res.end(JSON.stringify({'message': body}));
+      });     
+    }
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running 'hello-node' on port ${port}/`);
+});

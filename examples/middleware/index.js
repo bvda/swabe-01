c1ccbdcf968531de3fd57ef8656fddfc8534e778
Brian Vestergaard Danielsen <brian.v.danielsen@gmail.com>
2022-02-02 19:52:02 +0100

    Add error handling middleware

--- examples/middleware/index.js
+++ examples/middleware/index.js
@@ -4,31 +4,34 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+var errorHandler = (err, req, res, next) => {
+  const { message } = err;
+  console.error(message);
+  res.status(500).json({message});
+}
+
 app.use((req, res, next) => {
   if(!req.headers['authorization']) {
-    res.sendStatus(401)
+    res.sendStatus(401);
   }
-  next()
+  next();
 });
 
 app.get('/', (req, res) => {
-  res.json({'message' : 'Hello, Middleware!'})
+  res.json({'message' : 'Hello, Middleware!'});
 });
 
 app.get('/error', (req, res, next) => {
   setTimeout(() => {
     try {
-      throw new Error('/error')
+      throw new Error('Internal Server Error')
     } catch(err) {
-      next(err)
+      next(err);
     }
   }, 100);
 });
 
-app.use((err, req, res, next) => {
-  console.error(err);
-  next(err)
-});
+app.use(errorHandler);
 
 app.listen(port, _ => {
   console.log(`Server running 'middleware' on port ${port}`)

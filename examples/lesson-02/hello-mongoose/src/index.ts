78845d833c85e39c5dba2d1b81e02f4e05a4685d
Brian Vestergaard Danielsen <bvd@ase.au.dk>
2022-02-09 14:15:53 +0100

    Add test data and endpoint

--- examples/lesson-02/hello-mongoose/src/index.ts
+++ examples/lesson-02/hello-mongoose/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import { readFile } from 'fs/promises';
 
 import { Transaction, TransactionModel } from './transaction';
 
@@ -10,15 +11,31 @@ setImmediate(async () => {
   const db = await mongoose.connect('mongodb://localhost:27017')
 })
 
-
 app.get('', async (req, res) => {
-  let result = await TransactionModel.find<Transaction>({}).exec()
+  let result = await TransactionModel.find({}, { _id: 0, __v: 0 }).lean().exec()
   res.json(result);
 })
 
 app.post('', async (req, res) => {
   let { id } = await new TransactionModel(req.body).save()
   res.json({ id })
+})
+
+app.get('/search', async (req, res) => {
+  let query = req.query;
+  let result = await TransactionModel.find<Transaction>({ source: query.source }).exec()
+  res.json(result)
+})
+
+app.get('/bootstrap', async (req, res) => {
+  await TransactionModel.deleteMany({}).exec()
+  let data = await readFile('../assets/MOCK_DATA.json', 'utf-8');
+  let docs = await TransactionModel.insertMany(JSON.parse(data))
+  let ids = docs.map((t: Transaction) => t.id)
+  res.json({
+    cnt: ids.length,
+    ids
+  })
 })
 
 app.listen(3000, () => {
--- examples/lesson-02/hello-mongoose/src/transaction.ts
+++ examples/lesson-02/hello-mongoose/src/transaction.ts
@@ -2,15 +2,18 @@ import { Schema, model } from 'mongoose'
 
 export interface Transaction {
   id: string
-  source: string
-  destination:string
-  timestamp: Date
+  src: string
+  dst:string
+  amnt: number
+  ts: Date
 }
 
 const schema = new Schema<Transaction>({
-  source: { type: String, required: true },
-  destination: { type: String, required: true },
-  timestamp: { type: Date }
+  id: { type: String, required: true },
+  src: { type: String, required: true },
+  dst: { type: String, required: true },
+  amnt: { type: Number, required: true },
+  ts: { type: Date, required: true },
 })
 
 export const TransactionModel = model<Transaction>('transaction', schema)

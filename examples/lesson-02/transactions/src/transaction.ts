1c228536903670f78fef869b9bd029b41ecc24a6
Brian Vestergaard Danielsen <bvd@ase.au.dk>
2022-02-28 15:29:47 +0100

    Add custom validator

--- examples/lesson-02/transactions/src/transaction.ts
+++ examples/lesson-02/transactions/src/transaction.ts
@@ -5,14 +5,21 @@ const MONGO_DB_URL = 'mongodb://127.0.0.1:27017/transactions'
 
 export interface Transaction {
   timestamp: number
-  sender: string
+  sender: string  
   recipient: string
   amount: number
 }
 
+const walletValidation = {
+  validator: function validate(value: string) { 
+    return value.match('^(bc1|[13])[a-zA-HJ-NP-Z0-9]{25,39}$')
+  },
+  message: 'Not a valid Bitcoin address'
+}
+
 const schema = new Schema<Transaction>({
   timestamp: { type: Number, required: true },
-  sender: { type: String, required: true },
+  sender: { type: String, required: true, validate: walletValidation },
   recipient: { type: String, required: true },
   amount: { type: Number, required: true },
 })

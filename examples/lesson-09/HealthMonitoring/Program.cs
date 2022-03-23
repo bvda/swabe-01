d847253eb39c3839a5228793140a4d1236b72a89
Brian Vestergaard Danielsen <bvd@ase.au.dk>
2022-03-23 16:36:27 +0100

    Update port number and health check middleware

--- examples/lesson-09/HealthMonitoring/Program.cs
+++ examples/lesson-09/HealthMonitoring/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddHealthChecks();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -21,5 +21,6 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapHealthChecks("/healthz");
 
 app.Run();

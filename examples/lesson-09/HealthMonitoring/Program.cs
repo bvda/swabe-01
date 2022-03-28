72e7ca4c6aa8d8b830b0493bbbecd28b9771788d
Brian Vestergaard Danielsen <bvd@ase.au.dk>
2022-03-28 11:18:42 +0200

    Add checks

--- examples/lesson-09/HealthMonitoring/Program.cs
+++ examples/lesson-09/HealthMonitoring/Program.cs
@@ -5,15 +5,19 @@ using HealthChecks.UI.Client;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddHealthChecks().AddCheck<UsersHealthCheck>("Users").AddCheck<StartupHealthCheck>("Startup");
+builder.Services.AddHealthChecks()
+    .AddCheck<UsersHealthCheck>("Users", tags: new[] {"iam"})
+    .AddCheck<StartupHealthCheck>("Startup", tags: new[] {"infrastructure"});
+
+builder.Services.AddHealthChecksUI().AddInMemoryStorage();
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -25,13 +29,22 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-app.UseHealthChecks("/hc", new HealthCheckOptions()
+app.UseHealthChecksUI(config => config.UIPath = "/hc-ui");
+app.UseHealthChecks("/infrastructure", new HealthCheckOptions {
+    Predicate = x => x.Tags.Contains("infrastructure"),
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+app.UseHealthChecks("/users", new HealthCheckOptions {
+    Predicate = x => x.Tags.Contains("iam"),
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+app.MapHealthChecks("/healthz");
+app.MapHealthChecks("/live", new HealthCheckOptions()
 {
-    Predicate = _ => true,
+    Predicate = _ => false,
     ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
 });
 
 app.MapControllers();
-app.MapHealthChecks("/healthz");
 
 app.Run();
